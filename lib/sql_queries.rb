65f3c26fde355620f7d76aef78c6d5ccbd2edd68
Travis Exline <t.exline22@gmail.com>
2019-06-12 13:39:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,10 @@ GROUP BY projects.id HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECTS users.name, SUM(pledges.amount) FROM users
+INNER JOIN pledges ON users.id = pledges.user_id
+GROUP BY users.id
+ORDER BY pledges.amount"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
